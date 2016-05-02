cffbd135b1e407ac7d89b6999f982f7b38b63463
aaronleechan <aaron171091@gmail.com>
2016-05-02 15:36:52 -0700

    add

--- src/filePresenter.java
+++ src/filePresenter.java
@@ -9,25 +9,9 @@ public class filePresenter {
 	void loadfileReader()
 	{
 		fileReader = new readFile();
+		fileReader.read();
 		
-		// Read the file
-		System.out.println("Read File");
-		String filename = new Scanner(System.in).nextLine();
 		
-		try {    		
-			Scanner in = new Scanner(new File(filename), "UTF-8");
-			
-			while(in.hasNext())
-			{
-				String s = in.nextLine();
-				fileReader.read(s);
-			}			
-			
-		} catch (FileNotFoundException e) {
-		
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	  
 		
 		fileReader.readFile();
 	}
--- src/readFile.java
+++ src/readFile.java
@@ -1,12 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class readFile {
 	
 	ArrayList<String> storeAllList = new ArrayList<String>();
 	//Only to read a file
 	
-	public void read(String s) {
-		storeAllList.add(s);
+	public void read() {
+		
+		// Read the file
+				System.out.println("Read File");
+				String filename = new Scanner(System.in).nextLine();
+				
+				try {    		
+					Scanner in = new Scanner(new File(filename), "UTF-8");
+					
+					while(in.hasNext())
+					{
+						String s = in.nextLine();
+						storeAllList.add(s);
+					}			
+					
+				} catch (FileNotFoundException e) {
+				
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	  
+		
+		
 		// TODO Auto-generated method stub	
 	}
 	
