7f505326e3bf73338d83ebf260166b5eb2e75d57
aaronleechan <aaron171091@gmail.com>
2016-05-02 14:10:35 -0700

    addReader

--- src/filePresenter.java
+++ src/filePresenter.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class filePresenter {
 	FrameWork view;
@@ -6,6 +9,25 @@ public class filePresenter {
 	void loadfileReader()
 	{
 		fileReader = new readFile();
+		
+		// Read the file
+		System.out.println("Read File");
+		String filename = new Scanner(System.in).nextLine();
+		
+		try {    		
+			Scanner in = new Scanner(new File(filename), "UTF-8");
+			
+			while(in.hasNext())
+			{
+				String s = in.nextLine();
+				fileReader.read(s);
+			}			
+			
+		} catch (FileNotFoundException e) {
+		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	  
 		
 		
 		
--- src/readFile.java
+++ src/readFile.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 
 public class readFile {
 	
+	ArrayList<String> storeAllList = new ArrayList<String>();
 	//Only to read a file
+	
+	public void read(String s) {
+		storeAllList.add(s);
+		// TODO Auto-generated method stub	
+	}
 	
 	public void readFile(){
 		
