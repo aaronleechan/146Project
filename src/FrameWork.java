7722fea0f6c8b2a761910bef6e27148add65fd2a
aaronleechan <aaron171091@gmail.com>
2016-05-02 15:11:40 -0700

    frame

--- src/FrameWork.java
+++ src/FrameWork.java
@@ -23,10 +23,47 @@ public class FrameWork extends JFrame{
 		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		theFrame.setLayout(new BorderLayout());
 		
+		JPanel buttonKeys = new JPanel(new FlowLayout());
+		buttonKeys.add(new JLabel());
+		
+		JButton quickSortButton = new JButton("Sort height");
+		quickSortButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		JButton heapSortButton = new JButton("heap Sort");
+		heapSortButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		JButton mergeSortButton = new JButton("merge Sort");
+		mergeSortButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		buttonKeys.add(quickSortButton);
+		buttonKeys.add(heapSortButton);
+		buttonKeys.add(mergeSortButton);
 		
 		
-		JPanel title = new JPanel(new FlowLayout());
-		theFrame.add(title, BorderLayout.NORTH);
+		theFrame.add(buttonKeys, BorderLayout.NORTH);
 		theFrame.setSize(800, 900);
 		theFrame.setVisible(true);
 			
